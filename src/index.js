ce676d314fe2a6f1e4014d8935ca0f4547a58557
Leonid <ztvlnd777@gmail.com>
2023-04-05 21:25:59 +0300

    new

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function onSearch(e) {
     const KEY = '35072651-5af9f921021a18d865de9bf46';
           
     const url = 
- `https://pixabay.com/api/?key=${KEY}&q={query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page={perPage}`
+ `https://pixabay.com/api/?key=${KEY}&q={query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=${perPage}`
 
     fetch(url).then(r => r.json()).then(console.log)
     
