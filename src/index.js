5d3b6a58654c2dfef753e95d72427bc082f11761
Leonid <ztvlnd777@gmail.com>
2023-04-07 17:53:08 +0300

    new

--- src/index.js
+++ src/index.js
@@ -66,20 +66,22 @@ function renderGallery(images) {
     const markup = images.map(image => {
         const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image;
         return `
-         <a class="gallery-link" href="${largeImageURL}">
+        
             <div class="photo-card">
-               <img  class="gallery-item" src="${webformatURL}" alt="${tags}" loading="lazy" />
-               <div class="info">
-                 <p class="info-item"><b>Likes</b>${likes}</p>
-                 <p class="info-item"><b>Views</b>${views}</p>
-                 <p class="info-item"><b>Comments</b>${comments}</p>
-                 <p class="info-item"><b>Downloads</b>${downloads} </p>
-               </div>
+                <a href="${largeImageURL}">
+                     <img  class="gallery__image img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+                     <div class="info">
+                         <p class="info-item"><b>Likes</b>${likes}</p>
+                         <p class="info-item"><b>Views</b>${views}</p>
+                         <p class="info-item"><b>Comments</b>${comments}</p>
+                         <p class="info-item"><b>Downloads</b>${downloads} </p>
+                    </div>
+                </a>  
            </div>
-         </a> `
+          `
     }).join('');
    
-   refs.gallery.insertAdjacentHTML('beforebegin', markup);
+   refs.gallery.insertAdjacentHTML('beforeend', markup);
 
 }
 
